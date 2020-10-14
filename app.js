643ac1eb7281f17b97ff02a34220f56c43eed1da
Laura Lujan <lauralujanhdez@gmail.com>
2020-10-14 12:56:32 -0500

    Added js, css and html files, started basic books form

---
+++ app.js
@@ -0,0 +1,10 @@
+let myLibrary = [];
+
+function Book(id, title, author, pages, status){
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
